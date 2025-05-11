c18ceed07e83fdc156ce5f6e2f44d82e13cc70f6
MaheshPaade <mahimahesh2943@gmail.com>
2025-05-12 01:42:22 +0530

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,38 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import UserBody from "./components/UserBody";
 import Verifier from "./components/Verifier";
 import Admin from "./components/Admin";
-// import LoanForm from "./components/LoanForm";
 import AuthForm from "./components/AuthForm";
-// import Navbar from "./components/Header";
 import Header from "./components/Header";
-// import Navbar from "./components/Navbar"; 
 
-// const Layout = () => (
-//   <>
-//     <Navbar /> {/* Navbar for all pages */}
-//     <Routes />
-//   </>
-// );
+const AppRoutes = () => {
+  const location = useLocation();
+
+  return (
+    <>
+      {/* Only show Header if not on "/" (i.e., login route) */}
+      {location.pathname !== "/" && <Header />}
+
+      <Routes>
+        <Route path="/" element={<AuthForm />} />
+        <Route path="/user" element={<UserBody />} />
+        <Route path="/verifier" element={<Verifier />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </>
+  );
+};
 
 const App = () => {
   return (
     <Router>
-      {/* Redirect from `/` to `/user` */}
-      <Header/>
-      <Routes>
-        {/* <Route path="/" element={<Navigate to="/user" replace />} /> */}
-        
-        {/* Main Layout with Navbar included */}
-          <Route path="/" element={<AuthForm/>} />
-          <Route path="/user" element={<UserBody />} />
-          <Route path="/verifier" element={<Verifier />} />
-          <Route path="/admin" element={<Admin />} />
-        
-      </Routes>
+      <AppRoutes />
     </Router>
   );
 };
